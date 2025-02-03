7f0311e01beb88782ead520a46a8364884e2d9f0
nnam01 <isthenam@naver.com>
2025-02-03 21:55:34 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/nnam01/MyHomePage/MyHomePageApplication.java
@@ -0,0 +1,13 @@
+package nnam01.MyHomePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomePageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nnam01/MyHomePage/MyHomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package nnam01.MyHomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
